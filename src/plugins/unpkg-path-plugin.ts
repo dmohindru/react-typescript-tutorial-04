fdfff865d1a72b63df281b2820750fd03809840c
Dhruv Mohindru <dmohindru@gmail.com>
2023-05-24 07:10:39 +1000

    Completed video 91

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+  name: 'filecache'
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -45,12 +50,28 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        // Check to see if we have already fetched this file
+        // and if it is in the cache
+        const cacheResult = await fileCache.getItem(args.path);
+
+        // if it is, return it immediately
+        if (cacheResult) {
+          return cacheResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+        
+
+        const result = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         };
+        
+        // store response in cache
+        await fileCache.setItem(args.path, request);
+
+        return result;
       });
     },
   };

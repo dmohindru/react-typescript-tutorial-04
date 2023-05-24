d4163989197864d3c903954cc82789641853583f
Dhruv Mohindru <dmohindru@gmail.com>
2023-05-25 07:08:46 +1000

    completed video 94

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,78 +1,36 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localforage from 'localforage';
 
-const fileCache = localforage.createInstance({
-  name: 'filecache'
-});
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+
+      // Handle root entry file of 'index.js'
+      build.onResolve({filter: /(^index\.js$)/}, () => {
+        return { path: 'index.js', namespace: 'a'}
+      });
+
+      // Handle relative paths in a modules
+      build.onResolve({filter: /^\.+\//}, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(
+            args.path,
+            'https://unpkg.com' + args.resolveDir + '/'
+          ).href,
+        };
+      })
+
+      // Handle main file of module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
-          };
-        }
-
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
         };
-
-        // else if (args.path === 'tiny-test-pkg') {
-        //   return {
-        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-        //     namespace: 'a',
-        //   };
-        // }
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: `
-              import React, {useState } from 'react';
-              
-              console.log(React, useState);
-            `,
-          };
-        }
-
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
-        const cacheResult = await fileCache.getItem(args.path);
-
-        // if it is, return it immediately
-        if (cacheResult) {
-          return cacheResult;
-        }
-
-        const { data, request } = await axios.get(args.path);
-        
-
-        const result = {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname
-        };
-        
-        // store response in cache
-        await fileCache.setItem(args.path, request);
-
-        return result;
-      });
+      
     },
   };
 };

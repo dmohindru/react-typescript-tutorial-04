cc87ed98a2a23482adbb172833db2f962613759b
Dhruv Mohindru <dmohindru@gmail.com>
2023-06-01 07:13:17 +1000

    Completed till video 127

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const App = () => {
     const iframe = useRef<any>();
 
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
+    
 
     const startService = async() => {
         ref.current = await esbuild.startService({
@@ -28,6 +28,8 @@ const App = () => {
         if (!ref.current) {
             return;
         }
+
+        iframe.current.srcdoc = html;
 
         const result = await ref.current.build({
             entryPoints: ['index.js'],
@@ -54,7 +56,13 @@ const App = () => {
             <div id="root"></div>
             <script>
                 window.addEventListener('message', (event) => {
-                    eval(event.data);
+                    try{
+                        eval(event.data);
+                    } catch (err) {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>' + err + '</div>';
+                        console.error(err);
+                    }
                 }, false);
             </script>
         </body>
@@ -67,8 +75,7 @@ const App = () => {
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
-            <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+            <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
         </div>
     );
 };

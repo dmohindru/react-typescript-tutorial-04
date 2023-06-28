b426d854e46ea785dfe9bb85d6667e18639b10ef
Dhruv Mohindru <dmohindru@gmail.com>
2023-06-29 07:12:47 +1000

    Making progress

--- src/state/action-creator/index.ts
+++ src/state/action-creator/index.ts
@@ -34,7 +34,9 @@ export const moveCell = (id: string, direction: Direction): MoveCellAction => {
     };
 };
 
-export const insertCellBefore = (id: string, cellType: CellTypes): InsertCellBeforeAction => {
+export const insertCellBefore = (
+        id: string | null, 
+        cellType: CellTypes): InsertCellBeforeAction => {
     return {
         type: ActionType.INSERT_CELL_BEFORE,
         payload: {

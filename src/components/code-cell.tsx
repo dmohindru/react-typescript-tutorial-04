8428b3fa0dbd7a7640f72e12d7e432a8e510906c
Dhruv Mohindru <dmohindru@gmail.com>
2023-07-10 07:14:03 +1000

    Started new section

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -15,16 +15,44 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useTypedSelector((state) => {
+    const { data, order} = state.cells;
+    const orderedCells = order.map(id => data[id]);
+
+    const cumulativeCode = [
+      `
+        const show = (value) => {
+          if (typeof value === 'object') {
+            document.querySelector('#root').innerHTML = JSON.stringify(value);  
+          } else {
+            document.querySelector('#root').innerHTML = value;
+          }
+        }
+      `
+    ];
+    for (let c of orderedCells) {
+      if (c.type === 'code') {
+        cumulativeCode.push(c.content);
+      }
+      if (c.id === cell.id) {
+        break;
+      }
+    }
+
+    return cumulativeCode;
+  });
+
+  console.log(cumulativeCode);
 
   useEffect(() => {
 
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n'));
       return;
     }
     
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n'));
       
     }, 750);
 
@@ -32,7 +60,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle])
+  }, [cumulativeCode.join('\n'), cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
